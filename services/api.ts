4e9b4022c682bf9257e6e20395252b690c178bd9
Peeyush <peeyush1404@gmail.com>
2025-03-19 22:17:03 +0530

    Initial commit

---
+++ services/api.ts
@@ -0,0 +1,16 @@
+// const url = 'https://imdb236.p.rapidapi.com/imdb/tt7631058';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '167d0685c4msh7c0e41c852037acp193bc8jsn5cf45a29e162',
+// 		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// git branch
